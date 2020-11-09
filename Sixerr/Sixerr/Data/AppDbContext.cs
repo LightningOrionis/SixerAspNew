4e291aacf0cd39a1c4081faccc41a430fac29dbc
LightningOrionis <krasovsky17igor@gmail.com>
2020-11-09 03:29:22 +0300

    fixing shit

--- Sixerr/Sixerr/Data/AppDbContext.cs
+++ Sixerr/Sixerr/Data/AppDbContext.cs
@@ -13,6 +13,7 @@ namespace Sixerr.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
+            Database.EnsureCreated();
         }
 
         public DbSet<Gig> Gigs { get; set; }
