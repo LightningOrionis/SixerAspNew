902d468b49179a81793c0c859b56c1cc223baf88
LightningOrionis <krasovsky17igor@gmail.com>
2020-11-16 03:44:54 +0300

    startup fix

--- Sixerr/Sixerr/Startup.cs
+++ Sixerr/Sixerr/Startup.cs
@@ -41,9 +41,15 @@ namespace Sixerr
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
-            app.UseExceptionHandler("/Exception");
-            app.UseStatusCodePagesWithReExecute("/Error/{0}");
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Exception");
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");
+            }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
